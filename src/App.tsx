80f3c4ff98bc520916ef9ebfc842780eb8cdc60d
jonah-d6 <jonah.dean@maine.edu>
2026-02-02 11:52:46 -0500

    WebBook Chapters 2 and 3.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import React from "react";
+import {Button} from "react-bootstrap";
+import {Container} from "react-bootstrap";
+import {Row} from "react-bootstrap";
+import {Col} from "react-bootstrap";
 import "./App.css";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                <h1>UM COS420 with React Hooks and TypeScript</h1>
+                <Button onClick={ () => { console.log("Hello World!") } }>
+                  Log Hello World</Button>
             </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. My name is Jonah Dean. Hello World!
-            </p>
+            <Container>
+              <Row>
+                <Col>
+                  <p>
+                    My name is Jonah Dean. Hello World!
+                    Here&#39;s an image.
+                  </p>
+                  <img src="images/penguins-295133_640.png"
+                       alt="Three penguins standing in a circle."></img>
+                  <div style = { { width:100, height:100,
+                    backgroundColor:"red" } }/>
+                </Col>
+                <Col>
+                  <p>Now have a list of 3 random letters!</p>
+                  <ol>
+                    <li>R</li>
+                    <li>E</li>
+                    <li>D</li>
+                  </ol>
+                </Col>
+              </Row>
+            </Container>
         </div>
     );
 }
